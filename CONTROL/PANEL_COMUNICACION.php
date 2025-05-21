22b5d4dc74397e286af5ab22e5b476eb9d132db3
irving12-ai <irving.sanchez@uicui.edu.mx>
2025-05-21 10:47:27 -0600

    redisenocomun

--- CONTROL/PANEL_COMUNICACION.php
+++ CONTROL/PANEL_COMUNICACION.php
@@ -25,20 +25,35 @@ $tabla = new CL_TABLA_REVISTA();
 $revistas = $tabla->listar_todas();
 
 $html_revistas = "";
-foreach ($revistas as $r) {
+$html_revistas = "";
+
+$html_revistas .= "<div class=\"revistas-grid\">";
+
+foreach ($revistas as $index => $r) {
+    $portada = "../IMG/portada1.png"; // Asegúrate de tener una imagen por defecto o dinámica
+    $delay = ($index + 1) * 0.1; // Para animaciones
     $html_revistas .= "
-        <div>
-            <h2>{$r['titulo']}</h2>
-            <p>{$r['contenido']}</p>
-            <p><strong>Fecha:</strong> {$r['fecha_publicacion']}</p>
-            <p><strong>Autor:</strong> {$r['autor']}</p>
-            <a href=\"{$r['archivo_pdf']}\" class=\"btn-pdf\" target=\"_blank\">
-              <i class=\"fas fa-file-pdf\"></i> Ver PDF
-            </a>
-            <hr>
+    <div class=\"revista-card\" style=\"animation-delay: {$delay}s\">
+        <div class=\"revista-portada\">
+            <img src=\"$portada\" alt=\"Portada de la revista\">
         </div>
-    ";
+        <div class=\"revista-content\">
+            <div class=\"revista-titulo\">{$r['titulo']}</div>
+            <div class=\"revista-descripcion\">{$r['contenido']}</div>
+            <div class=\"revista-fecha\"><strong>Publicado:</strong> {$r['fecha_publicacion']}<br><strong>Autor:</strong> {$r['autor']}</div>
+            <div class=\"revista-acciones\">
+                <a href=\"{$r['archivo_pdf']}\" class=\"btn-pdf\" target=\"_blank\">
+                    <i class=\"fas fa-file-pdf\"></i> Ver PDF
+                </a>
+                <form method=\"POST\" action=\"../CONTROL/ELIMINAR_REVISTA.php\">
+                    <input type=\"hidden\" name=\"id_revista\" value=\"{$r['id_revista']}\">
+                </form>
+            </div>
+        </div>
+    </div>";
 }
+
+$html_revistas .= "</div>"; // Cierra la cuadrícula
 
 $vista = new CL_INTERFAZ12();
 $vista->mostrar($html_revistas);
